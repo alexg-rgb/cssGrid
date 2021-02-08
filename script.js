a70f768092731891724d8b1b7682502fd7d0a7b4
alexg-rgb <garcaoalexandre@gmail.com>
2021-02-08 09:16:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+livingPlace = prompt("Tell me where you live \n 3 questions left");
+reasonWhy = prompt("Now tell me why you live there \n 2 questions left");
+greatPlaceQ = prompt("And do you like this place \n 1 questions left");
+favouriteTea = prompt("What's your favourite tea ? \n Last one thanks");
+storyTeller = (greatPlaceQ) +", "+ (favouriteTea)+", "+(livingPlace) +", "+ (reasonWhy);
+alert(storyTeller);
+var t = 
+document.getElementById("firstCell").innerHTML = "Bonjour";
+document.getElementById("firstCell").style.color = "yellow";
